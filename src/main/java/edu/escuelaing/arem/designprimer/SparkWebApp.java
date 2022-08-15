260c9c9618099e6eabec4b3fb8b81e2e2de46b30
laura valentina <jennyyana@hotmail.com>
2022-08-15 17:16:33 -0500

    listo para desplegar

---
+++ src/main/java/edu/escuelaing/arem/designprimer/SparkWebApp.java
@@ -0,0 +1,17 @@
+package edu.escuelaing.arem.designprimer;
+import static spark.Spark.*;
+
+public class SparkWebApp {
+    public static void main(String[] args) {
+
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
+
